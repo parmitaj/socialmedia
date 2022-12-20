8693f29bf4bb1e7e7b67ee9ba6ee17ef224a2b9e
parmitaj <jhaparmita@gmail.com>
2022-12-20 05:54:00 +0000

    Added sample code for spring boot project
    Added sample code for spring boot project

---
+++ src/main/java/com/socialmedia/SocialMediaController.java
@@ -0,0 +1,15 @@
+package com.socialmedia;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SocialMediaController {
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "Hello from Social Media!";
+    }
+
+}
